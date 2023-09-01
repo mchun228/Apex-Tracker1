21f22298787883f6ee49b2917c6bc9f4c2eba8af
Michael Chun <mchun228@gmail.com>
2023-09-01 19:52:38 -0400

    updated ui to add logo

---
+++ client/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template> 
+    <header>
+        <img :src = "image" />
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header-Item",
+    data() {
+        return {
+            image: require('@/assets/logo.png')
+        };
+    }
+};
+</script>
+<style scoped>
+    img{
+        width: 300px;
+    }
+    @media(max-width: 700px){
+        img{
+            display: block; 
+            margin: auto;
+        }
+    }
+</style>
