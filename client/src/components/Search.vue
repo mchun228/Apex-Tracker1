4d70b83f6b88fe564addab9b732f1c4d6c735d30
Michael Chun <mchun228@gmail.com>
2023-09-02 07:52:13 -0400

    search bar

---
+++ client/src/components/Search.vue
@@ -0,0 +1,8 @@
+<template>
+    <section class = "search">
+        <h1>Tracker Player Stats</h1>
+        <form v-on:submit.prevent="onSubmit">
+            
+        </form>
+    </section>
+</template>
