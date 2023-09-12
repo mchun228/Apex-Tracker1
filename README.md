b0f90bf4062c2da62e57ee645f706223665f3661
Michael Chun <mchun228@gmail.com>
2023-09-12 17:46:38 -0400

    finished the search function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+Apex Legends Stat Tracker that uses tracker.gg API to fetch data.
 launch server: npm run dev
+ 
--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,30 +1,58 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
+<template>
+  <section class="search">
+    <h1>Track Player Stats</h1>
+    <form v-on:submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="platform">Platform</label>
+        <select name="platform" id="platform" v-model="platform">
+          <option value="psn">Playstation</option>
+          <option value="xbl">Xbox</option>
+          <option value="Origin">Origin</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="gamertag">Gamertag</label>
+        <input
+          type="text"
+          name="text"
+          v-model="gamertag"
+          id="gamertag"
+          placeholder="Origin ID, Xbox Live gamertag, PSN ID, etc"
+        />
+      </div>
+      <div class="form-group">
+        <input type="submit" value="Submit" class="btn" />
+      </div>
+    </form>
+  </section>
+</template>
 
-export default defineComponent({
+<script>
+export default {
+  name: "Search",
   data() {
     return {
-      platform: '' as string, // Initialize with the appropriate data type
-      gamertag: '' as string,
+      platform: "psn",
+      gamertag: ""
     };
+  },
+  beforeCreate() {
+    document.body.className = "body-bg-image";
+  },
+  methods: {
+    onSubmit() {
+      if (!this.gamertag) {
+        this.$toasted.show("Please enter a gamertag", {
+          duration: 3000,
+          icon: "exclamation-circle"
+        });
+      } else {
+        this.$router.push(`/profile/${this.platform}/${this.gamertag}`);
+      }
+    }
   }
-});
+};
 </script>
-<template>
-    <section class = "search">
-        <h1>Tracker Player Stats</h1>
-        <form v-on:submit.prevent="onSubmit">
-            <div class = "form-group">
-                <label for="platform">Platform</label>
-                <select name="platform" id = "platform" v-model="platform">
-                    <option value="psn">Playstation</option>
-                    <option value="xbl">Xbox</option>
-                    <option value="Origin">Origin</option>
-                </select>
-            </div>
-            <div class="form-group"><a/div>
-                <label for="gamertag">Gamertag</label>
-                <input type="text" name="gamertag" id="gamertag" v-model="gamertag">
-        </form>
-    </section>
-</template>
+
+<style>
+</style>
